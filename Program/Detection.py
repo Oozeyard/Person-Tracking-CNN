4538bcfbd5fe192f70f9e59f602c1a047296255e
Galax416 <emmanuelfontaine416@gmail.com>
2024-10-30 18:56:36 +0100

    first detection test

--- Program/Detection.py
+++ Program/Detection.py
@@ -1,4 +1,18 @@
 from ultralytics import YOLO
+from VideoReader import VideoReader
+import cv2
 
 # Load the model
-model = YOLO("yolo11n.pt")
+model = YOLO("Models/yolo11n.pt")
+
+
+video_path = "../Vidéos/video_test_1.mp4"
+
+video = VideoReader(video_path)
+
+frame = video.get_frame(0)
+
+results = model(frame)
+
+# Display the image
+results[0].show() 
--- Program/Main.py
+++ Program/Main.py
@@ -1,6 +1,6 @@
 from VideoReader import VideoReader
 
-video_path = "Vidéos/video_test_1.mp4"
+video_path = "../Vidéos/video_test_1.mp4"
 
 video = VideoReader(video_path)
 print(video.get_frame_count())
